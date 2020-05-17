8e7c594250db411a5e269197550a69326e160c64
wizardmil <emilkolvigraun@gmail.com>
2020-05-17 10:23:41 +0200

    project and stubs

---
+++ org.xtext.mdsd.arduino.boardgenerator.ide/src-gen/org/xtext/mdsd/arduino/boardgenerator/ide/contentassist/antlr/internal/InternalIoTParser.java
@@ -0,0 +1,563 @@
+package org.xtext.mdsd.arduino.boardgenerator.ide.contentassist.antlr.internal;
+
+import java.io.InputStream;
+import org.eclipse.xtext.*;
+import org.eclipse.xtext.parser.*;
+import org.eclipse.xtext.parser.impl.*;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.parser.antlr.XtextTokenStream;
+import org.eclipse.xtext.parser.antlr.XtextTokenStream.HiddenTokens;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.AbstractInternalContentAssistParser;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.DFA;
+import org.xtext.mdsd.arduino.boardgenerator.services.IoTGrammarAccess;
+
+
+
+import org.antlr.runtime.*;
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
+
+@SuppressWarnings("all")
+public class InternalIoTParser extends AbstractInternalContentAssistParser {
+    public static final String[] tokenNames = new String[] {
+        "<invalid>", "<EOR>", "<DOWN>", "<UP>", "RULE_ID", "RULE_INT", "RULE_STRING", "RULE_ML_COMMENT", "RULE_SL_COMMENT", "RULE_WS", "RULE_ANY_OTHER", "'Hello'", "'!'"
+    };
+    public static final int RULE_ID=4;
+    public static final int RULE_WS=9;
+    public static final int RULE_STRING=6;
+    public static final int RULE_ANY_OTHER=10;
+    public static final int RULE_SL_COMMENT=8;
+    public static final int RULE_INT=5;
+    public static final int T__11=11;
+    public static final int RULE_ML_COMMENT=7;
+    public static final int T__12=12;
+    public static final int EOF=-1;
+
+    // delegates
+    // delegators
+
+
+        public InternalIoTParser(TokenStream input) {
+            this(input, new RecognizerSharedState());
+        }
+        public InternalIoTParser(TokenStream input, RecognizerSharedState state) {
+            super(input, state);
+             
+        }
+        
+
+    public String[] getTokenNames() { return InternalIoTParser.tokenNames; }
+    public String getGrammarFileName() { return "InternalIoT.g"; }
+
+
+    	private IoTGrammarAccess grammarAccess;
+
+    	public void setGrammarAccess(IoTGrammarAccess grammarAccess) {
+    		this.grammarAccess = grammarAccess;
+    	}
+
+    	@Override
+    	protected Grammar getGrammar() {
+    		return grammarAccess.getGrammar();
+    	}
+
+    	@Override
+    	protected String getValueForTokenName(String tokenName) {
+    		return tokenName;
+    	}
+
+
+
+    // $ANTLR start "entryRuleModel"
+    // InternalIoT.g:53:1: entryRuleModel : ruleModel EOF ;
+    public final void entryRuleModel() throws RecognitionException {
+        try {
+            // InternalIoT.g:54:1: ( ruleModel EOF )
+            // InternalIoT.g:55:1: ruleModel EOF
+            {
+             before(grammarAccess.getModelRule()); 
+            pushFollow(FOLLOW_1);
+            ruleModel();
+
+            state._fsp--;
+
+             after(grammarAccess.getModelRule()); 
+            match(input,EOF,FOLLOW_2); 
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+        }
+        return ;
+    }
+    // $ANTLR end "entryRuleModel"
+
+
+    // $ANTLR start "ruleModel"
+    // InternalIoT.g:62:1: ruleModel : ( ( rule__Model__GreetingsAssignment )* ) ;
+    public final void ruleModel() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalIoT.g:66:2: ( ( ( rule__Model__GreetingsAssignment )* ) )
+            // InternalIoT.g:67:2: ( ( rule__Model__GreetingsAssignment )* )
+            {
+            // InternalIoT.g:67:2: ( ( rule__Model__GreetingsAssignment )* )
+            // InternalIoT.g:68:3: ( rule__Model__GreetingsAssignment )*
+            {
+             before(grammarAccess.getModelAccess().getGreetingsAssignment()); 
+            // InternalIoT.g:69:3: ( rule__Model__GreetingsAssignment )*
+            loop1:
+            do {
+                int alt1=2;
+                int LA1_0 = input.LA(1);
+
+                if ( (LA1_0==11) ) {
+                    alt1=1;
+                }
+
+
+                switch (alt1) {
+            	case 1 :
+            	    // InternalIoT.g:69:4: rule__Model__GreetingsAssignment
+            	    {
+            	    pushFollow(FOLLOW_3);
+            	    rule__Model__GreetingsAssignment();
+
+            	    state._fsp--;
+
+
+            	    }
+            	    break;
+
+            	default :
+            	    break loop1;
+                }
+            } while (true);
+
+             after(grammarAccess.getModelAccess().getGreetingsAssignment()); 
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "ruleModel"
+
+
+    // $ANTLR start "entryRuleGreeting"
+    // InternalIoT.g:78:1: entryRuleGreeting : ruleGreeting EOF ;
+    public final void entryRuleGreeting() throws RecognitionException {
+        try {
+            // InternalIoT.g:79:1: ( ruleGreeting EOF )
+            // InternalIoT.g:80:1: ruleGreeting EOF
+            {
+             before(grammarAccess.getGreetingRule()); 
+            pushFollow(FOLLOW_1);
+            ruleGreeting();
+
+            state._fsp--;
+
+             after(grammarAccess.getGreetingRule()); 
+            match(input,EOF,FOLLOW_2); 
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+        }
+        return ;
+    }
+    // $ANTLR end "entryRuleGreeting"
+
+
+    // $ANTLR start "ruleGreeting"
+    // InternalIoT.g:87:1: ruleGreeting : ( ( rule__Greeting__Group__0 ) ) ;
+    public final void ruleGreeting() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalIoT.g:91:2: ( ( ( rule__Greeting__Group__0 ) ) )
+            // InternalIoT.g:92:2: ( ( rule__Greeting__Group__0 ) )
+            {
+            // InternalIoT.g:92:2: ( ( rule__Greeting__Group__0 ) )
+            // InternalIoT.g:93:3: ( rule__Greeting__Group__0 )
+            {
+             before(grammarAccess.getGreetingAccess().getGroup()); 
+            // InternalIoT.g:94:3: ( rule__Greeting__Group__0 )
+            // InternalIoT.g:94:4: rule__Greeting__Group__0
+            {
+            pushFollow(FOLLOW_2);
+            rule__Greeting__Group__0();
+
+            state._fsp--;
+
+
+            }
+
+             after(grammarAccess.getGreetingAccess().getGroup()); 
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "ruleGreeting"
+
+
+    // $ANTLR start "rule__Greeting__Group__0"
+    // InternalIoT.g:102:1: rule__Greeting__Group__0 : rule__Greeting__Group__0__Impl rule__Greeting__Group__1 ;
+    public final void rule__Greeting__Group__0() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalIoT.g:106:1: ( rule__Greeting__Group__0__Impl rule__Greeting__Group__1 )
+            // InternalIoT.g:107:2: rule__Greeting__Group__0__Impl rule__Greeting__Group__1
+            {
+            pushFollow(FOLLOW_4);
+            rule__Greeting__Group__0__Impl();
+
+            state._fsp--;
+
+            pushFollow(FOLLOW_2);
+            rule__Greeting__Group__1();
+
+            state._fsp--;
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__0"
+
+
+    // $ANTLR start "rule__Greeting__Group__0__Impl"
+    // InternalIoT.g:114:1: rule__Greeting__Group__0__Impl : ( 'Hello' ) ;
+    public final void rule__Greeting__Group__0__Impl() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalIoT.g:118:1: ( ( 'Hello' ) )
+            // InternalIoT.g:119:1: ( 'Hello' )
+            {
+            // InternalIoT.g:119:1: ( 'Hello' )
+            // InternalIoT.g:120:2: 'Hello'
+            {
+             before(grammarAccess.getGreetingAccess().getHelloKeyword_0()); 
+            match(input,11,FOLLOW_2); 
+             after(grammarAccess.getGreetingAccess().getHelloKeyword_0()); 
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__0__Impl"
+
+
+    // $ANTLR start "rule__Greeting__Group__1"
+    // InternalIoT.g:129:1: rule__Greeting__Group__1 : rule__Greeting__Group__1__Impl rule__Greeting__Group__2 ;
+    public final void rule__Greeting__Group__1() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalIoT.g:133:1: ( rule__Greeting__Group__1__Impl rule__Greeting__Group__2 )
+            // InternalIoT.g:134:2: rule__Greeting__Group__1__Impl rule__Greeting__Group__2
+            {
+            pushFollow(FOLLOW_5);
+            rule__Greeting__Group__1__Impl();
+
+            state._fsp--;
+
+            pushFollow(FOLLOW_2);
+            rule__Greeting__Group__2();
+
+            state._fsp--;
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__1"
+
+
+    // $ANTLR start "rule__Greeting__Group__1__Impl"
+    // InternalIoT.g:141:1: rule__Greeting__Group__1__Impl : ( ( rule__Greeting__NameAssignment_1 ) ) ;
+    public final void rule__Greeting__Group__1__Impl() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalIoT.g:145:1: ( ( ( rule__Greeting__NameAssignment_1 ) ) )
+            // InternalIoT.g:146:1: ( ( rule__Greeting__NameAssignment_1 ) )
+            {
+            // InternalIoT.g:146:1: ( ( rule__Greeting__NameAssignment_1 ) )
+            // InternalIoT.g:147:2: ( rule__Greeting__NameAssignment_1 )
+            {
+             before(grammarAccess.getGreetingAccess().getNameAssignment_1()); 
+            // InternalIoT.g:148:2: ( rule__Greeting__NameAssignment_1 )
+            // InternalIoT.g:148:3: rule__Greeting__NameAssignment_1
+            {
+            pushFollow(FOLLOW_2);
+            rule__Greeting__NameAssignment_1();
+
+            state._fsp--;
+
+
+            }
+
+             after(grammarAccess.getGreetingAccess().getNameAssignment_1()); 
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__1__Impl"
+
+
+    // $ANTLR start "rule__Greeting__Group__2"
+    // InternalIoT.g:156:1: rule__Greeting__Group__2 : rule__Greeting__Group__2__Impl ;
+    public final void rule__Greeting__Group__2() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalIoT.g:160:1: ( rule__Greeting__Group__2__Impl )
+            // InternalIoT.g:161:2: rule__Greeting__Group__2__Impl
+            {
+            pushFollow(FOLLOW_2);
+            rule__Greeting__Group__2__Impl();
+
+            state._fsp--;
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__2"
+
+
+    // $ANTLR start "rule__Greeting__Group__2__Impl"
+    // InternalIoT.g:167:1: rule__Greeting__Group__2__Impl : ( '!' ) ;
+    public final void rule__Greeting__Group__2__Impl() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalIoT.g:171:1: ( ( '!' ) )
+            // InternalIoT.g:172:1: ( '!' )
+            {
+            // InternalIoT.g:172:1: ( '!' )
+            // InternalIoT.g:173:2: '!'
+            {
+             before(grammarAccess.getGreetingAccess().getExclamationMarkKeyword_2()); 
+            match(input,12,FOLLOW_2); 
+             after(grammarAccess.getGreetingAccess().getExclamationMarkKeyword_2()); 
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__Group__2__Impl"
+
+
+    // $ANTLR start "rule__Model__GreetingsAssignment"
+    // InternalIoT.g:183:1: rule__Model__GreetingsAssignment : ( ruleGreeting ) ;
+    public final void rule__Model__GreetingsAssignment() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalIoT.g:187:1: ( ( ruleGreeting ) )
+            // InternalIoT.g:188:2: ( ruleGreeting )
+            {
+            // InternalIoT.g:188:2: ( ruleGreeting )
+            // InternalIoT.g:189:3: ruleGreeting
+            {
+             before(grammarAccess.getModelAccess().getGreetingsGreetingParserRuleCall_0()); 
+            pushFollow(FOLLOW_2);
+            ruleGreeting();
+
+            state._fsp--;
+
+             after(grammarAccess.getModelAccess().getGreetingsGreetingParserRuleCall_0()); 
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Model__GreetingsAssignment"
+
+
+    // $ANTLR start "rule__Greeting__NameAssignment_1"
+    // InternalIoT.g:198:1: rule__Greeting__NameAssignment_1 : ( RULE_ID ) ;
+    public final void rule__Greeting__NameAssignment_1() throws RecognitionException {
+
+        		int stackSize = keepStackSize();
+        	
+        try {
+            // InternalIoT.g:202:1: ( ( RULE_ID ) )
+            // InternalIoT.g:203:2: ( RULE_ID )
+            {
+            // InternalIoT.g:203:2: ( RULE_ID )
+            // InternalIoT.g:204:3: RULE_ID
+            {
+             before(grammarAccess.getGreetingAccess().getNameIDTerminalRuleCall_1_0()); 
+            match(input,RULE_ID,FOLLOW_2); 
+             after(grammarAccess.getGreetingAccess().getNameIDTerminalRuleCall_1_0()); 
+
+            }
+
+
+            }
+
+        }
+        catch (RecognitionException re) {
+            reportError(re);
+            recover(input,re);
+        }
+        finally {
+
+            	restoreStackSize(stackSize);
+
+        }
+        return ;
+    }
+    // $ANTLR end "rule__Greeting__NameAssignment_1"
+
+    // Delegated rules
+
+
+ 
+
+    public static final BitSet FOLLOW_1 = new BitSet(new long[]{0x0000000000000000L});
+    public static final BitSet FOLLOW_2 = new BitSet(new long[]{0x0000000000000002L});
+    public static final BitSet FOLLOW_3 = new BitSet(new long[]{0x0000000000000802L});
+    public static final BitSet FOLLOW_4 = new BitSet(new long[]{0x0000000000000010L});
+    public static final BitSet FOLLOW_5 = new BitSet(new long[]{0x0000000000001000L});
+
+}
